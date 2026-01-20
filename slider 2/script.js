dc59a0987cff9d7140aa6f24c98591941f48fde7
mahiman1506 <mahimangaming39@gmail.com>
2026-01-20 15:06:45 +0530

    auto slide added in second slider

--- slider 2/script.js
+++ slider 2/script.js
@@ -2,13 +2,37 @@
 
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
+let autoSlideInterval;
+const autoSlideDuration = 3000; // 3 seconds
 
-next.addEventListener("click", function () {
+function slideNext() {
   let items = document.querySelectorAll(".item");
   document.querySelector(".slide").appendChild(items[0]);
+}
+
+function slidePrev() {
+  let items = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(items[items.length - 1]);
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(slideNext, autoSlideDuration);
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval);
+  startAutoSlide();
+}
+
+next.addEventListener("click", function () {
+  slideNext();
+  resetAutoSlide();
 });
 
 prev.addEventListener("click", function () {
-  let items = document.querySelectorAll(".item");
-  document.querySelector(".slide").prepend(items[items.length - 1]);
+  slidePrev();
+  resetAutoSlide();
 });
+
+// Start auto-slide on page load
+startAutoSlide();
